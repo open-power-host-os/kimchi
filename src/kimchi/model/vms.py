778ff289036905e1dea54ad04fe2b9fbf930bc70
Aline Manera <alinefm@linux.vnet.ibm.com>
2014-08-27 22:10:26 -0300

    Update iSCSI volume XML when creating a VM from an iSCSI pool
    When creating a VM using a iSCSI volume, the volume XML was like below:
    
    <disk type='volume' device='disk'>
      <driver name='qemu' type='raw'/>
      <source pool='TEST-ISCSI1' volume='unit:0:0:2' mode='host'/>
      <target dev='vda' bus='virtio'/>
      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
    </disk>
    
    But to attach a new iSCSI volume, the XML is:
    
    <disk type='block' device='disk'>
      <driver name='qemu' type='raw'/>
      <source dev='/dev/disk/by-id/wwn-0x60000000000000000e00000000010001'/>
      <target dev='vdb' bus='virtio'/>
      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
    </disk>
    
    We should keep the same XML structure in both cases to avoid problems
    while listing the storage volumes and getting their information.
    To solve that, this patch uses the latter XML also while creating a VM
    from a iSCSI volume.
    
    It eliminates the needs to have the authentication setting in the
    volume, because that the _get_storage_auth() function was replaced by
    _get_volume_path()
    
    Signed-off-by: Aline Manera <alinefm@linux.vnet.ibm.com>
    (cherry picked from commit e6931d7245cced2227f05c28742c77253bda413c)
    
    Conflicts:
    	src/kimchi/model/vms.py

--- src/kimchi/model/vms.py
+++ src/kimchi/model/vms.py
@@ -200,9 +200,7 @@ class VMsModel(object):
         # If storagepool is SCSI, volumes will be LUNs and must be passed by
         # the user from UI or manually.
         vol_list = []
-        if t._get_storage_type() in ["iscsi", "scsi"]:
-            vol_list = []
-        else:
+        if t._get_storage_type() not in ["iscsi", "scsi"]:
             vol_list = t.fork_vm_storage(vm_uuid)
 
         graphics = params.get('graphics')
