8d0e9a563bffc2454858e8826224baa4d49d0ba5
Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>
2015-03-05 19:32:39 -0300

    Fix pop up errors when log out at "Host" tab
    This patch fix the error that kimchi cannot log out from "Host" page
    and log in again to "Host".
    
    Signed-off-by: Rodrigo Trujillo <rodrigo.trujillo@linux.vnet.ibm.com>

--- src/kimchi/root.py
+++ src/kimchi/root.py
@@ -109,7 +109,7 @@ class Root(Resource):
         if page.endswith('.html'):
             context = template.render('tabs/' + page, data)
             cherrypy.response.cookie[
-                "lastPage"] = "/#tabs/" + page.rstrip(".html")
+                "lastPage"] = "/#tabs/" + page[:-5]
             cherrypy.response.cookie['lastPage']['path'] = '/'
             return context
         raise cherrypy.HTTPError(404)
