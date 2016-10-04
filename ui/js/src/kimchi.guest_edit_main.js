bfe8c182b4c40bd3478907301565f10c74932bc8
Lucio Correia <luciojhc@linux.vnet.ibm.com>
2016-10-04 16:13:22 -0300

    [HostOS] Revert "Enable CPU HotPlug in UI"
    This reverts commit 2f2b5ccdec881be5aef9fcaf62bd7ae7da5c8287.
    Feature will not be supported on HostOS 1.0.

--- ui/js/src/kimchi.guest_edit_main.js
+++ ui/js/src/kimchi.guest_edit_main.js
@@ -743,14 +743,15 @@ kimchi.guest_edit_main = function() {
             wok.window.open('plugins/kimchi/guest-storage-add.html', 'extendCreateStorage');
         });
         if ((kimchi.thisVMState === "running") || (kimchi.thisVMState === "paused")) {
-            $("#form-guest-edit-general input").not("#guest-edit-cores-textbox").prop("disabled", true);
-            $("#guest-edit-cores-hotplug-unsupported").removeClass('hidden');
             if (kimchi.capabilities.mem_hotplug_support) {
-                $("#guest-edit-memory-textbox").prop("disabled", false);
+                $("#form-guest-edit-general input").not("#guest-edit-memory-textbox").prop("disabled", true);
+            } else {
+                $("#form-guest-edit-general input").prop("disabled", true);
             }
-            else {
-                $("#guest-edit-memory-hotplug-unsupported").removeClass('hidden');
-            }
+        }
+        if (! kimchi.capabilities.mem_hotplug_support) {
+            $("#guest-edit-max-memory-textbox").prop("disabled", true);
+            $("#guest-edit-memory-hotplug-unsupported").removeClass('hidden');
         }
 
         $('#guest-show-max-memory').on('click', function(e) {
