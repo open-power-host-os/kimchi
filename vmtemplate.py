503494d3691846405b11f1ba75e72665ff5df2bb
Ramon Medeiros <ramonn@linux.vnet.ibm.com>
2016-10-11 15:32:02 -0300

    Use tablet_bus for tablet input instead of kbd_bus
    Signed-off-by: Ramon Medeiros <ramonn@linux.vnet.ibm.com>

--- vmtemplate.py
+++ vmtemplate.py
@@ -342,7 +342,7 @@ class VMTemplate(object):
         """
 
         tablet = """
-            <input type='tablet' bus='%(kbd_bus)s'> </input>
+            <input type='tablet' bus='%(tablet_bus)s'> </input>
         """
 
         video = """
